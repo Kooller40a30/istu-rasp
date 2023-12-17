8da7b0e808f3691bf3ddfaf8e62d3f2d0343ec0a
Danil Smolentsev <coming.dan@yandex.ru>
2023-12-17 15:26:09 +0400

    change selectize css to bootstrap5

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/selectize/dist/css/selectize.default.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/selectize/dist/css/selectize.bootstrap5.css') }}">
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.4/xlsx.full.min.js"></script>
     <script src="assets/microplugin/src/microplugin.js"></script>
