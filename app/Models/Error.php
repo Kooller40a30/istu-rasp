604def8146337cc4c19f348128656e92fae9c159
Danil Smolentsev <coming.dan@yandex.ru>
2024-10-06 17:11:52 +0400

    журнал логов

--- app/Models/Error.php
+++ app/Models/Error.php
@@ -11,4 +11,22 @@ class Error extends Model
     protected $table = 'errors';
 
     protected $guarded = false;
+
+    public static function getAttributeLabels() 
+    {
+        return [
+            'id' => 'ID ошибки',
+            'file' => 'Файл',
+            'group' => 'Группа',
+            'day' => 'День недели',
+            'week' => 'Неделя над/под чертой',
+            'class' => 'Пара',
+            'value' => 'Ошибка',
+            'teacher' => 'Преподаватель',
+            'classroom' => 'Аудитория',
+            'discipline' => 'Дисциплина',
+            'created_at' => 'Дата создания',
+            'updated_at' => 'Дата обновления',
+        ];
+    } 
 }
